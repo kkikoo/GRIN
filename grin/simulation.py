abae3a9618631f1e43c860d8995b8fbe31bbeaa5
Shiyi Mu <smu2@uci.edu>
2022-11-14 02:21:59 -0800

    create simulation: imports class errormessageclass & simulation

--- grin/simulation.py
+++ grin/simulation.py
@@ -6,6 +6,7 @@ from grin.compare import compare
 from grin.token import GrinTokenKind, GrinToken
 from grin.variable_list import VariableList
 from grin.arithmetic import Arithmetic
+from enum import Enum
 
 class ErrorMessageClass():
     """ Store all types of error-messgae
