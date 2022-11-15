d900808aa4617b13578b1fe2def8f73a167a835d
Shiyi Mu <smu2@uci.edu>
2022-11-14 20:48:51 -0800

    update project3.py

--- project3.py
+++ project3.py
@@ -10,11 +10,33 @@
 # offloading as much of the complexity as you can into additional modules in
 # the 'grin' package, isolated in a way that allows you to unit test them.
 
-import grin
+from grin.simulation import Simulation
+from grin.token import GrinToken
+from grin.parsing import parse
 
+def read_input_from_shell() -> list[list[GrinToken]]:
+    """ Get user's input from the PowerShell, and process the input to a list of GrinToken.
+    Returns:
+        list: The processed input, of type GrinToken
+    """
+    lines = []
+    while True:
+        line = input().strip()
+        if line == '.':
+            break
+        try:
+            line = list(parse([line]))[0]
+        except:
+            print("Error input in '{}', Program exit!".format(line))
+            break
+        lines.append(line)
+    return lines
 
-def main() -> None:
-    pass
+def main():
+    """do the simulation of Grin"""
+    lines = read_input_from_shell()
+    Sim = Simulation(lines)
+    Sim.simulation()
 
 
 if __name__ == '__main__':
