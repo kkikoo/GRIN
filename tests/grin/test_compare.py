bf94cfc2838b42a82ddcccb5ceec1247b09a0be5
Shiyi Mu <smu2@uci.edu>
2022-11-14 19:23:40 -0800

    create test_compare imports...

---
+++ tests/grin/test_compare.py
@@ -0,0 +1,7 @@
+# test_compare.py
+# unittest for the functions compare()
+
+from grin.parsing import parse
+from collections import defaultdict
+from grin.compare import compare
+from grin.variable_list import VariableList
